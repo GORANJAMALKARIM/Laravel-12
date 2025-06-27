fa3ed0a5bdec27e042c1fbb5a47150038df50c7b
Goran Jamal <70722651+GORANJAMALKARIM@users.noreply.github.com>
2025-06-27 22:57:26 +0200

    Create Layouts

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.public')
+@section('content')
+    <div class="container">
+        <h1>Welcome to the About Page</h1>
+        <p>This is the main content area of the about page.</p>
+    </div>
