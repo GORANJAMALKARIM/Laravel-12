7f7ce9230dac4e32b275c0490d239d05921cb39d
Goran Jamal <70722651+GORANJAMALKARIM@users.noreply.github.com>
2025-06-28 11:31:25 +0200

    Rout (Public, only, except ... und so weiter)

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
-
+use App\Http\Controllers\PublicRecourceController;
+use Dom\Comment;
 
 Route::get('/home',[PublicController::class ,'index'])->name('home');
 
 Route::get('/about',[PublicController::class ,'about'])->name('about');
+
+Route::resource('public', PublicRecourceController::class)->names([
+    'index' => 'public.index',
+    'create' => 'public.create',
+    'store' => 'public.store',
+    'show' => 'public.show',
+    'edit' => 'public.edit',
+    'update' => 'public.update',
+    'destroy' => 'public.destroy',
+]); 
+Route::resource('public', PublicRecourceController::class)->only([
+    'index' => 'public.index',
+    'create' => 'public.create',
+    'store' => 'public.store',
+
+]); 
+Route::resource('public', PublicRecourceController::class)->except([
+    'index' => 'public.index',
+    'create' => 'public.create',
+    'store' => 'public.store',
+
+]); 
