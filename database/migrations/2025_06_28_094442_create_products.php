ad81c3184996a50a178df1a726f8c0cb235184d3
Goran Jamal <70722651+GORANJAMALKARIM@users.noreply.github.com>
2025-07-01 14:25:41 +0200

    Migrate Table

--- database/migrations/2025_06_28_094442_create_products.php
+++ database/migrations/2025_06_28_094442_create_products.php
@@ -13,12 +13,18 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable(); 
+            $table->double('price')->nullable();
+            
+            // Name of the product
             $table->timestamps();
         });
     }
 
     /**
-     * Reverse the migrations.
+     * Reverse the migrations.wenn wir die Migration rückgängig machen wollen, löschen wir die Tabelle.
+     * This method is called when rolling back the migration.
+     * It drops the 'products' table.
      */
     public function down(): void
     {
